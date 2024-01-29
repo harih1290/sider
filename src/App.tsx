c38e62e0e791a20fea1ea51190b722d3226662d4
harih1290 <harih1290@gmail.com>
2024-01-30 02:10:45 +0530

    commit message

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import FixedBottomNavigation from './component/nav/FixedBottomNavigation';
+function App() {
+  return (
+    <div>
+      
+      <FixedBottomNavigation/>
+    </div>
+  );
+}
+
+export default App;
